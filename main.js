ec1425615de7be683b7ae06e18e529cc45c1e08e
keshavsharma1508 <85341193+keshavsharma1508@users.noreply.github.com>
2022-03-24 09:12:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ function draw()
 
      function check_sketch(){
          timer_counter++;
-         document.getElementById("time").innerHTML = "Timer : " + timer_counter;
+         document.getElementById("timer").innerHTML = "Timer : " + timer_counter;
          console.log(timer_counter)
          if(timer_counter>400){
              timer_counter = 0;
